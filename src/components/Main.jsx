db163be13bc27fbb365bb4b51851291a99556582
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-03 01:26:02 +0900

    feat: 그림자 안 생기는 위치 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,34 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useHelper } from "@react-three/drei";
 import { Character } from "./Character";
 import { Map } from "./Map";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import * as THREE from "three";
 import { CharacterController } from "./CharacterController";
 
 export const Main = () => {
   const [position, setPosition] = useState([0, 0, 0]);
+
+  const lightRef = useRef();
+
+  useHelper(lightRef, THREE.DirectionalLightHelper, 10);
   return (
     <>
       {/* <OrbitControls /> */}
       <directionalLight
-        position={[7, 7, 7]}
+        ref={lightRef}
+        position={[140, 100, 140]}
         castShadow
-        shadow-camera-left={-150} // left bound
-        shadow-camera-right={150} // right bound
-        shadow-camera-top={150} // top bound
-        shadow-camera-bottom={-150} // bottom bound
+        shadow-camera-left={-70} // X축 최소값, 평면의 왼쪽 끝 이하로 설정
+        shadow-camera-right={140} // X축 최대값, 평면의 오른쪽 끝 이상으로 설정
+        shadow-camera-top={140} // Z축 최대값, 평면의 상단 끝 이상으로 설정
+        shadow-camera-bottom={-70} // Z축 최소값, 평면의 하단 끝 이하로 설정
         shadow-camera-near={1} // near bound
-        shadow-camera-far={500} // far bound
+        shadow-camera-far={400} // far bound
+        shadow-mapSize-width={4096}
+        shadow-mapSize-height={4096}
       />
+
+      {/* <pointLight position={[7, 7, 7]} castShadow /> */}
 
       <Environment preset="sunset" />
       <CharacterController
