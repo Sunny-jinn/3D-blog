caace6096054ac9eed5694d31babf3e8b2a8dbf8
Sunny-jinn <rlawlsdn316@gmail.com>
2023-11-06 00:44:00 +0900

    feat: 한글 폰트 추가

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,11 +2,13 @@ import { Text } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
 import { useAtom } from "jotai";
 import React, { useState } from "react";
-import { touchAtom } from "./CharacterController";
 import { useSpring, a, animated } from "@react-spring/three";
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
+
 import { Wall } from "./Wall";
+import font1 from "/fonts/font1.ttf";
+import { touchAtom } from "../hooks/useCharacterControls";
 
 export const Map = ({ setPosition }) => {
   /**
@@ -21,7 +23,7 @@ export const Map = ({ setPosition }) => {
   // 애니메이션의 opacity 값을 제어합니다.
   const { opacity } = useSpring({
     opacity: isTouched ? 1 : 0,
-    config: { duration: 4000 },
+    config: { duration: 2000 },
     onRest: () => {
       // opacity가 0이 될 때 컴포넌트를 더 이상 렌더링하지 않도록 설정합니다.
       if (!isTouched) setShowText(false);
@@ -77,34 +79,34 @@ export const Map = ({ setPosition }) => {
       {/* 아래쪽 벽 */}
       <Wall position={[151, 0, 100]} args={[2, 5, 100]} />
 
-      {/* <RigidBody
+      <RigidBody
         colliders="trimesh"
         type="fixed"
-        position={[15, 5, 5]}
+        position={[60, 0, 50]}
         name="testBox"
       >
         <mesh>
-          <boxGeometry args={[10, 10, 10]} />
+          <boxGeometry args={[5, 5, 5]} />
           <meshStandardMaterial />
         </mesh>
       </RigidBody>
       <RigidBody
         colliders="trimesh"
         type="fixed"
-        position={[15, 5, 15]}
+        position={[70, 2, 50]}
         name="testSphere"
       >
         <mesh>
-          <sphereGeometry args={[5, 32, 32]} />
+          <sphereGeometry args={[2, 32, 32]} />
           <meshStandardMaterial />
         </mesh>
       </RigidBody>
       {showText && (
-        <Text position={[20, 5, 20]} fontSize={20}>
-          HI
-          <AnimatedMaterial color="#000" opacity={opacity} transparent />
+        <Text font={font1} position={[70, 5, 55]} fontSize={5}>
+          안녕하세용
+          <AnimatedMaterial color="#ffff00" opacity={opacity} transparent />
         </Text>
-      )} */}
+      )}
     </>
   );
 };
